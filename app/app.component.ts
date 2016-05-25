aaff6cae55b8c8caf6feeae2b55b489e3067971f
Hristo Deshev <hristo@deshev.com>
2016-05-25 15:29:09 +0300

    Initial import

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: `
+<StackLayout>
+    <Image src="http://graph.facebook.com/4/picture?width=200&height=200"></Image>
+</StackLayout>
+`,
+})
+export class AppComponent {
+}
